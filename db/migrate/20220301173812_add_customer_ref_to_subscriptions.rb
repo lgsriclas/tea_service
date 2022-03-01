95ba2b5c558269d0b66d5af28d472bc53aa8df6c
Lesley Sanders <lesleyasanders@gmail.com>
2022-03-01 16:06:50 -0500

    Set up seeds and fix migrations

--- db/migrate/20220301173812_add_customer_ref_to_subscriptions.rb
+++ db/migrate/20220301173812_add_customer_ref_to_subscriptions.rb
@@ -1,5 +1,5 @@
 class AddCustomerRefToSubscriptions < ActiveRecord::Migration[6.0]
   def change
-    add_reference :subscriptions, :customer, null: false, foreign_key: true
+    add_reference :subscriptions, :customer, foreign_key: true
   end
 end
