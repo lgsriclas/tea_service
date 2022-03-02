5e1881321bbcad16e4ff12e38281f8c0415521c8
Lesley Sanders <lesleyasanders@gmail.com>
2022-03-02 09:52:29 -0500

    Add relationships to models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,11 @@
 class Subscription < ApplicationRecord
+  belongs_to :tea
+  belongs_to :customer
+
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+  validates :customer_id, presence: true
+  validates :tea_id, presence: true 
 end
